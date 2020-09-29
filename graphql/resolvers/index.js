9c34c5d65e21a0f173f5c686bb0a18ccecc52374
iida nozomi <nozomi@mbp.local>
2020-09-29 18:43:32 +0900

    バックエンド完成

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,24 @@
-const postResolvers = require('./posts');
-const userResolvers = require('./users');
+const postsResolvers = require('./posts');
+const usersResolvers = require('./users');
+const commentsResolvers = require('./comments');
 
 module.exports = {
+  Post: { //createした時は作動せず、getしたときに作動する
+    likeCount(parent) {
+      console.log(parent);
+      return parent.likes.length;
+    },
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
-    ...postResolvers.Query,
+    ...postsResolvers.Query,
   },
   Mutation: {
-    ...userResolvers.Mutation,
-    ...postResolvers.Mutation,
-  }
-}
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation,
+  },
+  Subscription: {
+    ...postsResolvers.Subscription,
+  },
+};
